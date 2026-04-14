6c266f25e910ca35875139c00f584a14d4a41bc0
SIDHARTHABIMANYU <sidharthabimanyu@users.noreply.github.com>
2026-04-14 18:05:03 +0530

    Deployment: Updated backend URL to ngrok HTTPS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const PROXY_URL = 'http://52.66.64.135:3001';
+const PROXY_URL = 'https://unmischievously-rheotropic-luca.ngrok-free.dev';
 const BACKEND_URL = window.BACKEND_URL || PROXY_URL;
 import { auth, db, signOut, onAuthStateChanged, doc, setDoc, getDoc, collection, addDoc, getDocs, deleteDoc } from './src/firebase.js';
 
